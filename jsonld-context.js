d90e2103eda8d851f1a1443743323d4e7bfa5566
Erika Pauwels <erika.pauwels@gmail.com>
2019-09-26 13:58:27 +0200

    Use published JSON-LD context instead of hardcoded one

--- jsonld-context.js
+++ jsonld-context.js
@@ -1,36 +1,5 @@
-const context = {
-  "besluit": "http://data.vlaanderen.be/ns/besluit#",
-  "prov": "http://www.w3.org/ns/prov#",
-  "dct": "http://purl.org/dc/terms/",
-  "muAccount": "http://mu.semte.ch/vocabularies/account/",
-  "meb": "http://rdf.myexperiment.org/ontologies/base/",
-  "foaf": "http://xmlns.com/foaf/0.1/",
-  "pav": "http://purl.org/pav/",
-  "adms": "http://www.w3.org/ns/adms#",
-  "organization": {
-    "@id": "pav:createdBy",
-    "@type": "@id"
-  },
-  "href": { "@type": "@id", "@id": "prov:atLocation"},
-  "submittedResource": { "@type": "@id", "@id": "dct:subject" },
-  "key": "muAccount:key",
-  "publisher": "pav:providedBy",
-  "uri": {
-    "@type": "@id",
-    "@id": "@id"
-  },
-  "status": {
-    "@type": "@id",
-    "@id": "adms:status"
-  }
-};
-
-/**
- * returns the default jsonld context
- * NOTE: uses a hardcoded context for the moment, probably should retrieve it once available
- */
 async function getContext() {
-  return context;
+  return 'https://lblod.data.gift/contexts/automatische-melding/v1/context.json';
 }
 
 export { getContext };
