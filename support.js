748429a6f3259691c3742ac9a20cb56a6bbc8e5c
Dries Hugaerts <dries.hugaerts@live.be>
2022-01-13 15:06:54 +0100

    Fixed broken query after introduction new js-template.

--- support.js
+++ support.js
@@ -358,7 +358,7 @@ async function sendErrorAlert({message, detail, reference}) {
                     dct:created ${sparqlEscapeDateTime(new Date().toISOString())} ;
                     dct:creator ${sparqlEscapeUri(CREATOR)} .
             ${reference ? `${sparqlEscapeUri(uri)} dct:references ${sparqlEscapeUri(reference)} .` : ''}        
-            ${detail ? `${sparqlEscapeUri(uri)} oslc:largePreview ""${sparqlEscapeString(detail)}"" .` : ''}        
+            ${detail ? `${sparqlEscapeUri(uri)} oslc:largePreview ${sparqlEscapeString(detail)} .` : ''}        
         }
       }
    `;
