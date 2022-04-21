e19f26934539bb6339d165584cf295e6ff13f1e5
Unknown <achrafatauil@hotmail.com>
2022-04-21 13:00:00 +0200

    add constants

---
+++ constants.js
@@ -0,0 +1,5 @@
+export const CREATOR = 'http://lblod.data.gift/services/automatic-submission-service';
+export const SUBMISSION_OPERATION = 'http://lblod.data.gift/id/jobs/concept/JobOperation/automaticSubmission';
+export const TASK_OPERATION_IMPORTING = 'http://lblod.data.gift/id/jobs/concept/TaskOperation/importing';
+export const STATUS_NOT_STARTED = 'http://lblod.data.gift/automatische-melding-statuses/not-started';
+export const STATUS_BUSY = 'http://redpencil.data.gift/id/concept/JobStatus/busy';
