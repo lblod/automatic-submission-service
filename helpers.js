d1ef31dc6fcdc81c87154467d9cb964c9e889dc9
benjay10 <ben.behaeghe@redpencil.io>
2022-08-08 22:24:10 +0200

    Removed unnecessary files
    A lot of this code could be grouped in another file or moved to a better
    place in favour of simplicity.

--- helpers.js
+++
@@ -1,8 +0,0 @@
-export function previousMap(collection, f) {
-  let result = [];
-  if (collection.length > 0)
-    result.push(f(collection[0], undefined));
-  for (let i = 1; i < collection.length; i++)
-    result.push(f(collection[i], collection[i - 1]));
-  return result;
-}
--- jsonld-context.js
+++
@@ -1,7 +0,0 @@
-import { AUTOMATIC_SUBMISSION_JSON_LD_CONTEXT_ENDPOINT } from './env';
-
-async function getContext() {
-  return AUTOMATIC_SUBMISSION_JSON_LD_CONTEXT_ENDPOINT;
-}
-
-export { getContext };
